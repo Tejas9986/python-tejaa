d1b1cce748671a08354e2b7973f043f3466f3cc0
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-07-13 15:49:44 +0000

    datatype solutions

---
+++ 01_dataType/03_dictionary.py
@@ -0,0 +1,18 @@
+# create a dictionary that give square of the number
+
+n = int(input("Enter the number: "))
+d = dict()
+
+for i in range(1, n+1):
+    d[i] = i*i
+
+print(d)
+
+
+
+# square
+# squares = {x: x*x for x in range(6)}
+
+# print(squares)
+
+
---
+++ 01_dataType/dataType.py
@@ -0,0 +1,15 @@
+# print("Hello, Tejas! Welcome in python space")
+
+# Question: Write a program which will find all such numbers which are divisible by 7 
+# but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be
+#  printed in a comma-separated sequence on a single line.
+
+
+numbers = []
+
+for i in range(100, 200):
+    if (i % 7 ==0) and (i % 5 != 0):
+        numbers.append(i)       
+# append is use to add the number or value at the in variable(number) and top add into it convert integer into string format
+
+print("Numbers: ", numbers)
